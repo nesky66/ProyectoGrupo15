c1cf60772b0dea9c5b15abbc0b86f38d86d00bff
nesky66 <nesky66@gmail.com>
2021-09-08 12:17:26 -0300

    reparaciones

--- PreguntasChaco/apps/usuarios/models.py
+++ PreguntasChaco/apps/usuarios/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Usuario(AbstractUser):
-	pass
+	score = models.IntegerField(default=0)
--- PreguntasChaco/apps/usuarios/views.py
+++ PreguntasChaco/apps/usuarios/views.py
@@ -6,6 +6,7 @@ def registerUser(request):
     if request.method=='POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data['username']
             messages.success(request,f'Usuario {username} creado con exito')
             return redirect('login')
